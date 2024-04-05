f4594acd3e8411b35047083f74c1f9f1845570e9
Yahya305 <saimyahya47@gmail.com>
2024-04-06 01:56:13 +0500

    feat(ui): add card

--- four-more-supports/src/components/pages/Home/index.tsx
+++ four-more-supports/src/components/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Cards from './Cards'
 
 function index() {
   return (
     <div>
       <Header  />
+      <Cards />
       <Footer />
     </div>
   )
