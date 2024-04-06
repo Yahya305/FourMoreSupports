87d07b3cc434e61c483c939bad0421acca48df42
Hamza Siddiqui <80615079+hamzapro305@users.noreply.github.com>
2024-04-06 06:10:07 +0500

    Content updated

---
+++ four-more-supports-frontend/src/Lib/Data/FooterMenu1.ts
@@ -0,0 +1,48 @@
+const FooterMenu1 = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+        name: "NDIS Support Services",
+        path: "/",
+    },
+    {
+        name: "About FMS",
+        path: "/",
+    },
+    {
+        name: "NDIS",
+        path: "/",
+    },
+    {
+        name: "Blogs",
+        path: "/",
+    },
+    {
+        name: "Contact us",
+        path: "/",
+    },
+    {
+        name: "Schedule of Supports",
+        path: "/",
+    },
+];
+const FooterMenu2 = [
+    {
+        name: "Terms of Use",
+        path: "/",
+    },
+    {
+        name: "Privacy Policy",
+        path: "/",
+    },
+    {
+        name: "Commplaints",
+        path: "/",
+    },
+];
+export const FooterMenu = {
+    FooterMenu1,
+    FooterMenu2,
+};
---
+++ four-more-supports-frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
+
+export default function Home() {
+    return (
+        <main>
+            <Footer />
+            <Header />
+        </main>
+    );
+}
