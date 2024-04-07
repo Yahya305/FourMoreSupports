214023d6830c97215984782dc1ae158cbf95ba79
Yahya305 <saimyahya47@gmail.com>
2024-04-07 07:58:08 +0500

    feat(fixes): fix cards

--- four-more-supports-frontend/src/Components/OurServices.tsx
+++ four-more-supports-frontend/src/Components/OurServices.tsx
@@ -6,8 +6,8 @@ function OurServices() {
     <section className='ourservices-container'>
         <div className='cards'>
             <Card variant='secondary' imgSrc='/Card1Logo.png' buttonContent='Learn More' headingContent='Support Coordination' descContent='Discover how our tailored Support Coordination can guide you through your NDIS journey, ensuring you leverage the full potential of your plan for a better, more empowered life in Adelaide.' />
-            <Card variant='primary' imgSrc='/Card1Logo.png' buttonContent='Learn More' headingContent='Support Coordination' descContent='Discover how our tailored Support Coordination can guide you through your NDIS journey, ensuring you leverage the full potential of your plan for a better, more empowered life in Adelaide.' />
-            <Card variant='secondary' imgSrc='/Card1Logo.png' buttonContent='Learn More' headingContent='Support Coordination' descContent='Discover how our tailored Support Coordination can guide you through your NDIS journey, ensuring you leverage the full potential of your plan for a better, more empowered life in Adelaide.' />
+            <Card variant='primary' imgSrc='/Card1Logo.png' buttonContent='Learn More' headingContent='Specialist Support Coordination' descContent='Elevate your NDIS journey with Four More Supports’ Specialist Support Coordination in Adelaide. Tailored, expert solutions for complex needs.' />
+            <Card variant='secondary' imgSrc='/Card1Logo.png' buttonContent='Learn More' headingContent='Psychosocial Recovery Coaching' descContent='Achieve mental health resilience and recovery with Four More Supports’ Psychosocial Recovery Coaching in Adelaide. Expert guidance every step of the way.' />
         </div>
       <div className='background-lines'></div>
     </section>
